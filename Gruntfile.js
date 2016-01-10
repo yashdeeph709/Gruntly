2ac949a9ac17d03a6cafdd922c0ab2a38748a03c
yashdeep hinge <yashdeeph709@gmail.com>
2016-01-11 03:12:24 +0530

    sass cache omitted and basic compilation from sass to css working!

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,14 @@
 module.exports = function(grunt) {
    grunt.initConfig({
-      log: {
-         foo: [1, 2, 3],
-         bar: 'hello world',
-         baz: false
+      sass: {
+         dist: {
+            files: {
+               'screen.css': 'screen.scss'
+            }
+         }
       }
    });
-   grunt.registerTask('foo', 'A sample task that logs stuff.', function(arg1, arg2) {
-      if (arguments.length === 0) {
-         grunt.log.writeln(this.name + ", no args");
-      } else {
-         grunt.log.writeln(this.name + ", " + arg1 + " " + arg2);
-      }
-   });
-   grunt.registerMultiTask('log', 'Log stuff.', function() {
-      grunt.log.writeln(this.target + ': ' + this.data);
-   }); // A very basic default task.
-
-   grunt.registerTask('default', 'Log some stuff.', ['log', 'foo:bar:mar']);
+   grunt.loadNpmTasks('grunt-contrib-sass');
+   grunt.registerTask('default', 'Log some stuff.', ['sass']);
 
 };
