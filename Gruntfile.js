d2aa9622fe13e9820eafbed8ffb2f1283b0b3db8
yashdeep hinge <yashdeeph709@gmail.com>
2016-01-11 23:46:22 +0530

    chrome opening on run

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,9 +2,16 @@ module.exports = function(grunt) {
    grunt.initConfig({
       serve: {
          path: 'Angular-D3'
+      },
+      open: {
+         dev: {
+            path: 'http://127.0.0.1:9000/Angular-D3/index.html',
+            app: 'chrome.exe'
+         }
       }
    });
    grunt.loadNpmTasks('grunt-serve');
-   grunt.registerTask('default', 'Log some stuff.', ['serve']);
+   grunt.loadNpmTasks('grunt-open');
+   grunt.registerTask('default', 'Log some stuff.', ['open', 'serve']);
 
 };
